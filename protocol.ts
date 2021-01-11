f532e640c948297f764dfd4a8dad6160b806c7af
Joel Hernandez <joel@open-book.org>
2021-01-11 22:52:36 +0100

    :sparkles: add left room event

--- protocol.ts
+++ protocol.ts
@@ -1,17 +1,18 @@
 const actions = {
-  AUTHENTICATE: 'authenticate',
-  JOIN_ROOM: 'joinRoom',
-  SEND_MESSAGE: 'sendMessage',
-  UPDATE_STATUS: 'updateStatus',
+  AUTHENTICATE: "authenticate",
+  JOIN_ROOM: "joinRoom",
+  SEND_MESSAGE: "sendMessage",
+  UPDATE_STATUS: "updateStatus",
 };
 
 const events = {
-  AUTHENTICATED: 'authenticated',
-  JOINED_ROOM: 'joinedRoom',
-  NEW_MESSAGE: 'newMessage',
-  USER_STATUS_UPDATE: 'userStatusUpdate',
-  ROOM_STATUS_UPDATE: 'roomStatusUpdate',
-  ERROR: 'error',
+  AUTHENTICATED: "authenticated",
+  JOINED_ROOM: "joinedRoom",
+  NEW_MESSAGE: "newMessage",
+  USER_LEFT_ROOM: "userLeftRoom",
+  USER_STATUS_UPDATE: "userStatusUpdate",
+  ROOM_STATUS_UPDATE: "roomStatusUpdate",
+  ERROR: "error",
 };
 
 export const PixlyProtocol = {
