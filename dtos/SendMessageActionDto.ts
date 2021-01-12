a482a5925195f8656fcae109f8b0b708e5fcce8b
Joel Hernandez <joel@open-book.org>
2021-01-12 19:44:24 +0100

    :recycle: change dtos attrs to be simpler

--- dtos/SendMessageActionDto.ts
+++ dtos/SendMessageActionDto.ts
@@ -1,6 +1,6 @@
-import { IsString } from 'class-validator';
+import { IsString } from "class-validator";
 
 export class SendMessageActionDto {
   @IsString()
-  public messageText: string;
+  public text: string;
 }
